ec87f691efde1df3dcc90d60d8f002666a3c61bf
matchima <mkhamyod@gmail.com>
2024-08-09 10:15:39 +0700

    add airline and passenger service

---
+++ src/services/AirlineService.ts
@@ -0,0 +1,17 @@
+import axios, { type AxiosResponse } from 'axios'
+import type { Airline } from '@/types'
+
+const apiClient = axios.create({
+    baseURL: 'https://my-json-server.typicode.com/se331-2022/passengerdb',
+    withCredentials: false,
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+    }
+})
+
+export default {
+    getAirlineById(id: number): Promise<AxiosResponse<Airline>> {
+        return apiClient.get<Airline>(`/airline/${id}`)
+    }
+}
---
+++ src/services/PassengerService.ts
@@ -0,0 +1,20 @@
+import axios, { type AxiosResponse } from 'axios'
+import type { Passenger } from '@/types'  // Adjust the import to match your folder structure
+
+const apiClient = axios.create({
+    baseURL: 'https://my-json-server.typicode.com/se331-2022/passengerdb',
+    withCredentials: false,
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+    }
+})
+
+export default {
+    getPassengers(perPage: number, page: number): Promise<AxiosResponse<Passenger[]>> {
+        return apiClient.get<Passenger[]>(`/passenger?_limit=${perPage}&_page=${page}`)
+    },
+    getPassengerById(id: number): Promise<AxiosResponse<Passenger>> {
+        return apiClient.get<Passenger>(`/passenger/${id}`)
+    }
+}
